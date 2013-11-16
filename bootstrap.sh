e5e278a822ef6b13ddebbbb2cb67c801e45458c2
Jesse Collier <jessecollier@gmail.com>
2013-11-15 16:15:37 -0800

    my first chef test

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,12 +8,15 @@
 # - Your base image includes a cronjob 
 #   that downloads and executes this bootstrap
 
+if [ ! -a /etc/cron.d/bootstrap ]; then
+    logger "[$$] [$0] exiting... already bootstrapped"
+fi
 
 logger "[$$] [$0] bootstrapping. Hang on..."
 
 # Upgrade system to latest
 logger "[$$] [$0] upgrade system"
-apt-get update
+apt-get update 
 apt-get upgrade
 logger "[$$] [$0] upgrade system complete"
 
@@ -22,8 +25,12 @@ logger "[$$] [$0] upgrade system complete"
 logger "[$$] [$0] install ruby+chef"
 apt-get install -y ruby ruby1.8-dev build-essential wget libruby1.8 rubygems
 gem update --no-rdoc --no-ri
-gem install ohai chef --no-rdoc --no-ri
 logger "[$$] [$0] ruby+chef installed"
+
+# Set up berkshelf
+logger "[$$] [$0] install bundler"
+gem install bundler
+logger "[$$] [$0] bundler installed"
 
 # Install git
 logger "[$$] [$0] install git"
@@ -34,9 +41,10 @@ logger "[$$] [$0] git installed"
 logger "[$$] [$0] setting up chef"
 mkdir /srv
 cd /srv && git clone https://github.com/jessecollier/configs.git
+cd /srv/configs && bundle install
+cd /srv/configs && berks install
 chef-solo -c /srv/configs/chef/config.rb
 logger "[$$] [$0] chef complete"
-
 
 logger "[$$] [$0] removing bootstrap"
 rm /etc/cron.d/bootstrap
